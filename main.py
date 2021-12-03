ace53a43fff60f3c210b81a72d8cdf98f5618528
AlinaDbeep <82227705+AlinaDbeep@users.noreply.github.com>
2021-12-03 04:05:45 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from datetime import datetime
 import telebot
 from auth_data import token
 
+token = "2111549416:AAHl847LbsQXZhQBx9InJ68O6tCWLIdn5RA"
+
 def get_data():
     req = requests.get("https://yobit.net/api/3/ticker/btc_usd")
     response = req.json()
